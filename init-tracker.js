783bb63013b80866be5fca6457444a2697ce6fd8
Todd Kennedy <todd@selfassembled.org>
2017-03-30 09:54:30 -0700

    choo5 is great

--- init-tracker.js
+++ init-tracker.js
@@ -1,5 +1,6 @@
 var choo = require('choo')
 var html = require('choo/html')
+var log = require('choo-log')
 var css = require('sheetify')
 
 var sf = css('./node_modules/tachyons/css/tachyons.css')
@@ -15,35 +16,74 @@ var st = css`
   .row:hover {
     background-color: cyan;
   }
+
+  .active {
+    background-color: brightred;
+  }
+
+  .num {
+    width: 3rem;
+  }
 `
 
 var app = choo()
 app.use(initStore)
+app.use(log())
 app.route('/', mainView)
 app.mount('body')
 
 function table (state, emit) {
-  console.log('hi i am here')
   var keys = Object.keys(state.init.order)
   keys.sort((a, b) => parseInt(a, 10) < parseInt(b, 10))
   return keys.map(key => {
     var chars = state.init.order[key]
-    return chars.map(chr => html`<div class="cf row"><span class="fl w-10">${key}</span><span class="fl w-90">${chr}</span></div>`)
+    var active = ''
+    if (state.init.current.score === key) {
+      active = 'active ba'
+    }
+
+    return chars.map(chr => html`<div class="cf row ${active}"><span class="fl w-10">${key}</span><span class="fl w-90">${chr}</span></div>`)
   })
 }
 
+function turnCounter (state, emit) {
+  return html`<div><span class="b">Turn:</span> ${state.init.turn}</div>`
+}
+
+function addCharacter (state, emit) {
+  return html`<section class="w-80 ma2 pa2 ba">
+    <h2>Add a character</h2>
+    <form onsubmit=${(evt) => evt.preventDefault()}>
+      <label>Initiative: <input autoFocus="true" type="number" max="30" min="-30" step="1" class="num" oninput=${(evt) => update('score', evt)} value="${state.init.score}"></label>
+      <label>Name: <input type="text" oninput=${(evt) => update('name', evt)} onkeydown=${submitOnEnter} value="${state.init.name}"></label>
+      <button onclick=${add}>Add</button>
+    </form>
+  </section>`
+
+  function submitOnEnter (evt) {
+    if (evt.key === 'Enter') {
+      add(evt)
+    }
+  }
+
+  function add (evt) {
+    emit('add', {score: state.init.score, name: state.init.name})
+    evt.preventDefault()
+  }
+
+  function update (key, evt) {
+    emit('input', {key, val: evt.target.value})
+  }
+}
+
 function mainView (state, emit) {
-  console.log('state is now', state)
   return html`
-  <body class="${sf} ${st}">
+  <body class="${sf} ${st}" onkeydown=${next}>
     <h1>Initiative Tracker</h1>
-    <section class="w-80 ma2 pa2 ba">
-      <h2>Add a character</h2>
-      <form onsubmit=${(evt) => evt.preventDefault()}>
-        <label>Initiative: <input type="number" oninput=${(evt) => update('score', evt.target.value)} value="${state.init.score}"></label>
-        <label>Name: <input type="text" oninput=${(evt) => update('name', evt.target.value)} value="${state.init.name}"></label>
-        <button onclick=${(evt) => emit('add', {score: state.init.score, name: state.init.name})}>Add</button>
-      </form>
+    ${state.init.started ? '' : addCharacter(state, emit)}
+    <section class="w-80 ma2">
+      <button onclick=${start}>Start!</button><button onclick=${end}>End!</button><br>
+      ${state.init.started ? turnCounter(state, emit) : ''}
     </section>
     <section class="w-80 ma2">
       <div class="cf bb mv2"><span class="fl w-10">Init</span><span class="fl w-90">Character</span></div>
@@ -51,9 +91,20 @@ function mainView (state, emit) {
     </section>
   </body>
   `
+  function start (evt) {
+    evt.preventDefault()
+    emit('start')
+  }
 
-  function update (key, val) {
-    emit('input', {key, val})
+  function end (evt) {
+    evt.preventDefault()
+    emit('end')
+  }
+
+  function next () {
+    if (state.init.started) {
+      emit('next')
+    }
   }
 }
 
@@ -61,20 +112,71 @@ function initStore (state, emitter) {
   if (!state.init) {
     state.init = {
       order: {},
+      current: {
+        score: null,
+        index: null
+      },
       name: '',
-      score: ''
+      score: '',
+      started: false,
+      turn: 0
     }
   }
+
+  emitter.on('next', function () {
+    state.init.current.index++
+    var sorted = sort()
+    if (state.init.current.index >= sorted.length) {
+      state.init.current.index = 0
+      state.init.turn++
+    }
+    state.init.current.score = sort()[state.init.current.index]
+    emitter.emit('render')
+  })
+
+  emitter.on('start', function () {
+    state.init.current.score = highest()
+    state.init.current.index = 0
+    state.init.started = true
+    emitter.emit('render')
+  })
+
+  emitter.on('end', function () {
+    state.init.started = false
+    state.init.current.score = null
+    state.init.current.index = null
+    state.init.turn = 0
+    emitter.emit('render')
+  })
 
   emitter.on('input', function (data) {
     state.init[data.key] = data.val
   })
 
   emitter.on('add', function (data) {
+    if (state.init.started) {
+      state.init.score = lowest()
+    }
+
     if (!Array.isArray(state.init.order[state.init.score])) {
       state.init.order[state.init.score] = []
     }
+
     state.init.order[state.init.score].push(state.init.name)
+    state.init.name = ''
+    state.init.score = ''
     emitter.emit('render')
   })
+
+  function sort () {
+    return Object.keys(state.init.order).sort((a, b) => parseInt(a, 10) < parseInt(b, 10))
+  }
+
+  function highest () {
+    return sort()[0]
+  }
+
+  function lowest () {
+    return Object.keys(state.init.order).sort((a, b) => parseInt(a, 10) > parseInt(b, 10))[0]
+  }
 }
