474865e294398144d4544cb0c023c63223a4cefa
Todd Kennedy <todd@selfassembled.org>
2017-04-03 07:24:53 -0700

    refactoring store into separate components

--- store.js
+++ store.js
@@ -1,72 +1,102 @@
+var shortid = require('shortid')
+
+function getActiveCharacter (state) {
+  var currInit = state.initiative[state.current.initScore]
+  var currInitIdx = currInit.findIndex(chr => chr.id === state.current.charId)
+  return currInitIdx
+}
+
+function getNextInitScore (state, emitter) {
+  var sorted = sort(state.initative)
+  var currentScoreIndex = sorted.findIndex(key => state.current.initScore === key)
+  var nextScoreIndex = currentScoreIndex + 1
+  if (nextScoreIndex >= sorted.length) {
+    emitter.emit('tracker:nextTurn')
+    return sorted[0]
+  }
+  return sorted[nextScoreIndex]
+}
+
+function sort (obj) {
+  return Object.keys(obj).sort((a, b) => parseInt(a, 10) < parseInt(b, 10))
+}
+
+function highest (obj) {
+  return sort(obj)[0]
+}
+
+function lowest (obj) {
+  var sorted = sort(obj)
+  return sorted[sorted.length - 1]
+}
+
+function nextCharacter (state, emitter) {
+  var nextChar = getActiveCharacter() + 1
+  if (nextChar >= state.initiative[state.current.initScore].length) {
+    state.current.initScore = getNextInitScore(state)
+    state.current.charId = state.initiative[state.current.initScore][0]
+  } else {
+    state.current.charId = state.initiative[state.current.initScore][nextChar]
+  }
+  emitter.emit('render')
+}
+
+function nextTurn (state, emitter) {
+  state.current.turn++
+  emitter.emit('render')
+}
+
+function start (state, emitter) {
+  state.current.initScore = highest()
+  state.current.index = 0
+  state.current.turn = 0
+  state.started = true
+  emitter.emit('render')
+}
+
+function end (state, emitter) {
+  state.started = false
+  state.current.initScore = null
+  state.current.index = 0
+  state.init.turn = 0
+  emitter.emit('render')
+}
+
+function add (state, emitter) {
+  if (state.started) {
+    state.entry.initScore = lowest()
+  }
+
+  var newCharacter = Object.assign({}, state.entry)
+  newCharacter.id = shortid.generate()
+  state.characters[newCharacter.id] = newCharacter
+  if (!Array.isArray(state.initiative[newCharacter.initScore])) {
+    state.initiative[newCharacter.initScore].push(newCharacter.id)
+  }
+  state.entry = Object.create(null)
+  emitter.emit('render')
+}
+
 module.exports = function initStore (state, emitter) {
-  if (!state.init) {
-    state.init = {
-      order: {},
-      current: {
-        score: null,
-        index: null
-      },
-      name: '',
-      score: '',
-      started: false,
-      turn: 0
+  state = {
+    characters: {}, // list of characters
+    initiative: {}, // key: init, val: array of characters at that init
+    entry: {}, // for recording new characters
+    active: false, // is the tracker active
+    current: { // record keeping
+      turn: 0,
+      charId: null,
+      initScore: 0
     }
   }
 
-  emitter.on('next', function () {
-    state.init.current.index++
-    var sorted = sort()
-    if (state.init.current.index >= sorted.length) {
-      state.init.current.index = 0
-      state.init.turn++
-    }
-    state.init.current.score = sort()[state.init.current.index]
-    emitter.emit('render')
+  emitter.on('tracker:nextCharacter', () => nextCharacter(state, emitter))
+  emitter.on('tracker:nextTurn', () => nextTurn(state, emitter))
+  emitter.on('tracker:start', () => start(state, emitter))
+  emitter.on('tracker:end', () => end(state, emitter))
+  emitter.on('character:add', () => add(state, emitter))
+
+  emitter.on('character:input', function (data) {
+    state.entry[data.key] = data.val
   })
-
-  emitter.on('start', function () {
-    state.init.current.score = highest()
-    state.init.current.index = 0
-    state.init.started = true
-    emitter.emit('render')
-  })
-
-  emitter.on('end', function () {
-    state.init.started = false
-    state.init.current.score = null
-    state.init.current.index = null
-    state.init.turn = 0
-    emitter.emit('render')
-  })
-
-  emitter.on('input', function (data) {
-    state.init[data.key] = data.val
-  })
-
-  emitter.on('add', function (data) {
-    if (state.init.started) {
-      state.init.score = lowest()
-    }
-
-    if (!Array.isArray(state.init.order[state.init.score])) {
-      state.init.order[state.init.score] = []
-    }
-
-    state.init.order[state.init.score].push(state.init.name)
-    state.init.name = ''
-    state.init.score = ''
-    emitter.emit('render')
-  })
-
-  function sort () {
-    return Object.keys(state.init.order).sort((a, b) => parseInt(a, 10) < parseInt(b, 10))
-  }
-
-  function highest () {
-    return sort()[0]
-  }
-
-  function lowest () {
-    return Object.keys(state.init.order).sort((a, b) => parseInt(a, 10) > parseInt(b, 10))[0]
-  }
 }
